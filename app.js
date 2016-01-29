8e759a826a003f27e03a0fdf966890101c437a76
Vishal Ranjan <vishivish18@gmail.com>
2016-01-29 17:22:40 +0530

    CORS

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var app = express();
 
 console.log("hello");
 //app.use(morgan('dev'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.use('/api/route', router);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
